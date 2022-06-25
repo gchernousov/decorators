1ea10649c51904f562cd8f1e8102963fdf1c5187
gchernousov <seasidesuicide@yandex.ru>
2022-06-25 11:23:28 +0500

    first commit

---
+++ log_decorator.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def log_decorator(file_path):
+    def add_log(old_func):
+        def new_func(*args, **kwargs):
+            arguments_in = args, kwargs
+            result = old_func(*args, **kwargs)
+            datetime.now()
+            req_line = f"{datetime.now()}: function: {old_func.__name__}, incoming arguments: {arguments_in}, result: {result}\n"
+            with open(file_path, "a") as file:
+                file.write(req_line)
+                file.close()
+            return result
+        return new_func
+    return add_log
---
+++ main.py
@@ -0,0 +1,36 @@
+from log_decorator import log_decorator
+
+# курс валют на 25.06.2022:
+CURRENCY_DICT = {
+    "USD": 53.32,
+    "EUR": 55.96,
+    "BRL": 10.29,
+    "INR": 0.68,
+    "KZT": 0.11,
+    "CNY": 8.02,
+    "PLN": 11.94,
+    "TRY": 3.07,
+    "UAH": 1.80,
+    "JPY": 0.39
+}
+
+
+@log_decorator("requests.log")
+def converter(RUB: int, currency: str):
+    if currency in CURRENCY_DICT:
+        v = CURRENCY_DICT[currency]
+        result = RUB / v
+        return round(result, 2)
+    else:
+        return f"Данных о валюте {currency} нет."
+
+
+if __name__ == "__main__":
+
+    print(converter(500, "USD"))
+    # print(converter(1000, "TRY"))
+    # print(converter(100, "EUR"))
+    # print(converter(25000, "CNY"))
+    # print(converter(9000, "ZBR"))
+    # print(converter(7000, "PLN"))
+    # print(converter(100000, "BRL"))
